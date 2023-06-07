b63a26b83bc2ae1227a52d5d9bf3d5dce802d886
engageintellect <jc9361@gmail.com>
2023-06-07 10:21:39 -0700

    cleaning up styles

--- src/lib/components/ProjectCard.svelte
+++ src/lib/components/ProjectCard.svelte
@@ -4,32 +4,33 @@
 </script>
 
 <div
-	class="card bg-base-100 shadow-lg rounded-lg group hover:saturate-150 hover:shadow-xl transition-all duration-200"
+	class="card border bg-base-100 rounded group hover:saturate-150 hover:shadow-lg transition-all duration-300"
 >
-	<a href="/projects/{project.id}" class="rounded-lg">
+	<a href="/projects/{project.id}" class="rounded">
 		<figure>
 			<img
-				class="rounded-t-lg h-56 w-full object-cover group-hover:scale-105 transition-transform duration-500"
+				class="rounded-t h-56 w-full object-cover group-hover:scale-105 transition-transform duration-500"
 				src={project?.thumbnail
 					? getImageURL(project.collectionId, project.id, project.thumbnail)
 					: `https://via.placeholder.com/500/4506CB/FFFFFF/?text=${project.name}`}
 				alt="project thumbnail"
 			/>
 		</figure>
-		<div class="card-body">
-			<h2 class="card-title">{project.name}</h2>
-			<!-- <div class="badge badge-secondary">{project.division}</div> -->
-
+		<div class="card-body p-4">
 			{#if project.division}
-				<div class="badge badge-info w-1/3 rounded-full py-3 gap-2">
+				<div class="badge badge-primary rounded py-3 gap-2">
 					{project.division}
 				</div>
 			{/if}
 
+			<h2 class="card-title">{project.name}</h2>
+			<!-- <div class="badge badge-secondary">{project.division}</div> -->
+
 			<p>{project.tagline}</p>
-			<div class="card-actions mt-5">
+
+			<!-- <div class="card-actions mt-5">
 				<a href="/projects/{project.id}" class="btn btn-outline w-full">View Project</a>
-			</div>
+			</div> -->
 		</div>
 	</a>
 </div>
