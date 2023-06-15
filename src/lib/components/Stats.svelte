fda035d1046433bdc5e7935eb0b7dd8e40e78b93
engageintellect <jc9361@gmail.com>
2023-06-15 02:34:55 -0700

    cleaning up dashboard styles

--- src/lib/components/Stats.svelte
+++ src/lib/components/Stats.svelte
@@ -2,6 +2,7 @@
 	export let avatar;
 	export let likes;
 	export let favorites;
+	export let projects;
 
 	import { Icon, Heart, HandThumbUp, Share } from 'svelte-hero-icons';
 </script>
@@ -9,17 +10,21 @@
 <div class="stats shadow w-full">
 	<div class="grid grid-cols-1 md:grid-cols-3">
 		<div class="stat">
-			<div class="stat-figure text-primary">
-				<Icon src={Heart} class="w-8 h-8" />
+			<div class="stat-figure text-secondary">
+				<div class="avatar online">
+					<div class="w-16 rounded-full shadow-md">
+						<img src={avatar} alt="user avatar" />
+					</div>
+				</div>
 			</div>
-			<div class="stat-title">Total Likes</div>
-			<div class="stat-value text-primary">{likes}</div>
-			<div class="stat-desc">21% more than last month</div>
+			<div class="stat-title">Projects</div>
+			<div class="stat-value">{projects}</div>
+			<div class="stat-desc text-secondary">31 tasks remaining</div>
 		</div>
 
 		<div class="stat">
 			<div class="stat-figure text-secondary">
-				<Icon src={HandThumbUp} class="w-8 h-8" />
+				<Icon src={Heart} class="w-8 h-8 text-error drop-shadow-md" solid />
 			</div>
 			<div class="stat-title">Favorites</div>
 			<div class="stat-value text-secondary">{favorites}</div>
@@ -27,16 +32,12 @@
 		</div>
 
 		<div class="stat">
-			<div class="stat-figure text-secondary">
-				<div class="avatar online">
-					<div class="w-16 rounded-full">
-						<img src={avatar} alt="user avatar" />
-					</div>
-				</div>
+			<div class="stat-figure text-primary">
+				<Icon src={HandThumbUp} class="w-8 h-8 text-info drop-shadow-md" solid />
 			</div>
-			<div class="stat-value">86%</div>
-			<div class="stat-title">Tasks done</div>
-			<div class="stat-desc text-secondary">31 tasks remaining</div>
+			<div class="stat-title">Total Likes</div>
+			<div class="stat-value text-primary">{likes}</div>
+			<div class="stat-desc">21% more than last month</div>
 		</div>
 	</div>
 </div>
--- src/routes/my/dashboard/+page.svelte
+++ src/routes/my/dashboard/+page.svelte
@@ -1,11 +1,20 @@
 <script>
 	import { Stats } from '$lib/components';
+	import { getImageURL } from '$lib/utils';
+	import { Icon, PencilSquare } from 'svelte-hero-icons';
 	export let data;
 
 	let count = {
 		likes: data.user.likes.length,
 		favorites: data.user.favorites.length
 	};
+
+	const projectCount = data.projects.reduce((count, project) => {
+		if (project.user === data.user.id) {
+			return count + 1;
+		}
+		return count;
+	}, 0);
 </script>
 
 <div class="mt-10 px-4">
@@ -14,16 +23,36 @@
 	</div>
 	<div class="divider" />
 
-	<Stats likes={count.likes} favorites={count.favorites} avatar={data.user.avatar} />
+	<Stats
+		projects={projectCount}
+		likes={count.likes}
+		favorites={count.favorites}
+		avatar={data.user?.avatar
+			? getImageURL(data.user?.collectionId, data.user?.id, data.user?.avatar)
+			: `https://ui-avatars.com/api/?name=${data.user?.name}`}
+	/>
 
 	<div class="grid grid-cols-1 md:grid-cols-3 gap-5 mt-5">
 		<div class="">
-			<div class="text-3xl font-bold">Projects</div>
+			<div class="text-3xl md:hidden">Projects</div>
 			<div class=" flex flex-col">
 				{#each data.projects as project}
 					{#if project.user === data.user.id}
-						<a class="p-5 bg-base-300 rounded my-2" href={`/projects/${project.id}`}>
-							{project.name}
+						<a
+							class="p-5 border border-primary hover:shadow-lg rounded my-2 transition-all duration-200"
+							href={`/projects/${project.id}`}
+						>
+							<div class="flex justify-between">
+								<div>
+									{project.name}
+								</div>
+
+								<div class="hover:scale-105 active:scale-95 transition-transform duration-200">
+									<a href={`/projects/${project.id}/edit`}>
+										<Icon src={PencilSquare} class="w-5 h-5 text-primary" solid />
+									</a>
+								</div>
+							</div>
 						</a>
 					{/if}
 				{/each}
@@ -31,12 +60,15 @@
 		</div>
 
 		<div class="">
-			<div class="text-3xl font-bold">Likes</div>
+			<div class="text-3xl md:hidden">Favorites</div>
 			<div class=" flex flex-col">
-				{#each data.user.likes as like}
+				{#each data.user.favorites as favorite}
 					{#each data.projects as project}
-						{#if project.id === like}
-							<a class="p-5 bg-base-300 rounded my-2" href={`/projects/${project.id}`}>
+						{#if project.id === favorite}
+							<a
+								class="p-5 border border-primary hover:shadow-lg rounded my-2 transition-all duration-200"
+								href={`/projects/${project.id}`}
+							>
 								{project.name}
 							</a>
 						{/if}
@@ -46,12 +78,15 @@
 		</div>
 
 		<div class="">
-			<div class="text-3xl font-bold">Favorites</div>
+			<div class="text-3xl md:hidden">Likes</div>
 			<div class=" flex flex-col">
-				{#each data.user.favorites as favorite}
+				{#each data.user.likes as like}
 					{#each data.projects as project}
-						{#if project.id === favorite}
-							<a class="p-5 bg-base-300 rounded my-2" href={`/projects/${project.id}`}>
+						{#if project.id === like}
+							<a
+								class="p-5 border border-primary hover:shadow-lg rounded my-2 transition-all duration-200"
+								href={`/projects/${project.id}`}
+							>
 								{project.name}
 							</a>
 						{/if}
